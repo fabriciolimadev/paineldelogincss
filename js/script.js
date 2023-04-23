351916fdd90e774c5998a6048e9debbb1e919dc8
Fabrício Lima <fabriciolimadeveloper@gmail.com>
2023-04-23 20:39:33 -0300

    Site pronto e responsivo

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const emailInput = document.querySelector('input[type="email"]');
+const passInput = document.querySelector('input[type="password"]');
+const emailIcon = document.querySelector(".icon-email");
+const passIcon = document.querySelector(".icon-pass");
+
+emailInput.addEventListener("focus", () => {
+  emailIcon.src = "/images/mail-focus.svg";
+});
+
+emailInput.addEventListener("blur", () => {
+  emailIcon.src = "/images/mail.svg";
+});
+
+passInput.addEventListener("focus", () => {
+  passIcon.src = "/images/lock-focus.svg";
+});
+
+passInput.addEventListener("blur", () => {
+  passIcon.src = "/images/lock.svg";
+});
+
+
+
+const eyeIcon = document.querySelector(".icon-eye");
+
+eyeIcon.addEventListener("click", () => {
+  if (passInput.type === "password") {
+    passInput.type = "text";
+  } else {
+    passInput.type = "password";
+  }
+});
